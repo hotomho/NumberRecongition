aceea1588fcf32c8d64e3d90684a137a2606491e
hth <bstc12039@gmail.com>
2020-11-12 19:59:28 +0800

    MNIST data

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from mnist import MNIST
-mndata = MNIST('../data')
-images, labels = mndata.load_training()
+import numpy as np
+import matplotlib.pyplot as plt
+
+trainingData = MNIST(path='./data/training', gz=True)
+trainingData_image, trainingData_labels = trainingData.load_training()
+testingData = MNIST(path='./data/testing', gz=True)
+testingData_image, testingData_labels = testingData.load_training()
+
+# test:
+for i in range(10):
+    img = np.reshape(testingData_image[i], (28, 28))
+    plt.imshow(img, cmap="Greys")
+    plt.title(testingData_labels[i])
+    plt.show()
